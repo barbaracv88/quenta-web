8d80ed1592631ac126cc1e2af12b9320f6c19a1c
BarbaracvApptiva <barbara.calabrese@apptivatech.com>
2020-03-06 12:34:50 +0100

    creacion pagina apm

---
+++ src/templates/apm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class apm extends Component {
+    render() {
+        return (
+            <div>
+                hola
+            </div>
+        );
+    }
+}
+
+export default apm;

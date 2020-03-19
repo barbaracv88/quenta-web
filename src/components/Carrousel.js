72c9e9a8026d5b22bf679e2d8a7d39f99a5ec692
BarbaracvApptiva <barbara.calabrese@apptivatech.com>
2020-03-19 17:55:04 +0100

    camnios prueba carousel

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -14,6 +14,7 @@ export default class Carrousel extends React.Component {
             infinite: true,
             centerPadding: "0px",
             slidesToShow: 4,
+            slidesToScroll: 4,
             speed: 500,
             // autoplay:true,
           
@@ -53,66 +54,69 @@ export default class Carrousel extends React.Component {
                 </div>
               <Slider {...settings}>
               <div className="employee"> 
-                  <a href="https://www.linkedin.com/in/arancha-s%C3%A1nchez-guti%C3%A9rrez-7391712b/">
-                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""/>
+              <div>
+                  <a href="https://www.linkedin.com/in/arancha-s%C3%A1nchez-guti%C3%A9rrez-7391712b/"  target="_blank">
+                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""></img>
                   </a>
                   <a href="mailto:arancha.sanchez@quenta.es">
-                    <img className="rrss mail" src = "images/rrss-43.png" alt=""/>
+                    <img className="rrss mail" src = "images/rrss-43.png" alt=""></img>
                   </a>
                        
-                  <img className="img_employee" src = "images/arancha.jpg" alt=""/>
+                  <img className="img_employee" src = "images/arancha.jpg" alt=""></img>
                   <p className="name">Arancha Sánchez</p>
                   <p className="position">Key Account Manager</p>
+                  </div>
               </div>
 
               <div className="employee"> 
-                  <a href="https://www.linkedin.com/in/antonio-villarroel-a3074724/?originalSubdomain=es">
-                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""/>
+                  <a href="https://www.linkedin.com/in/antonio-villarroel-a3074724/?originalSubdomain=es"  target="_blank">
+                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""></img>
                   </a>
                   <a href="mailto:ajvillarroel.work@quenta.es">
-                    <img className="rrss mail" src = "images/rrss-43.png" alt=""/>
+                    <img className="rrss mail" src = "images/rrss-43.png" alt=""></img>
                   </a>
                        
-                  <img className="img_employee" src = "images/antonio.jpg" alt=""/>
+                  <img className="img_employee" src = "images/antonio.jpg" alt=""></img>
                   <p className="name">Antonio Villarroel</p>
                   <p className="position">System Engineer</p>
               </div>
 
               <div className="employee"> 
-                  <a href="https://www.linkedin.com/in/juanjavierrodriguez/">
-                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""/>
+                  <a href="https://www.linkedin.com/in/juanjavierrodriguez/"  target="_blank">
+                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""></img>
                   </a>
                   <a href="mailto:javier.rodriguez@quenta.es">
-                    <img className="rrss mail" src = "images/rrss-43.png" alt=""/>
+                    <img className="rrss mail" src = "images/rrss-43.png" alt=""></img>
                   </a>
                        
-                  <img className="img_employee" src = "images/javier.jpg" alt=""/>
+                  <img className="img_employee" src = "images/javier.jpg" alt=""></img>
                   <p className="name">Javier Rodríguez</p>
                   <p className="position">Managing Director</p>
               </div>
 
               <div className="employee"> 
-                  <a href="https://www.linkedin.com/in/iv%C3%A1n-pastor-58320920/">
-                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""/>
+                  <a href="https://www.linkedin.com/in/iv%C3%A1n-pastor-58320920/"  target="_blank">
+                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""></img>
                   </a>
                   <a href="mailto:ivan.pastor@quenta.es">
-                    <img className="rrss mail" src = "images/rrss-43.png" alt=""/>
+                    <img className="rrss mail" src = "images/rrss-43.png" alt=""></img>
+                   
                   </a>
                        
-                  <img className="img_employee" src = "images/ivan.jpg" alt=""/>
+                  <img className="img_employee" src = "images/ivan.jpg" alt=""></img>
                   <p className="name">Iván Pastor</p>
                   <p className="position">Service Manager</p>
               </div>
 
               <div className="employee"> 
-                  <a href="https://www.linkedin.com/in/mar%C3%ADa-dolores-bola%C3%B1os-ortega-07a3334a/">
-                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""/>
+                  <a href="https://www.linkedin.com/in/mar%C3%ADa-dolores-bola%C3%B1os-ortega-07a3334a/"  target="_blank">
+                    <img className="rrss linkedin" src = "images/rrss-42.png" alt=""></img>
                   </a>
                   <a href="mailto:maria.bolaños@quenta.es">
-                    <img className="rrss mail" src = "images/rrss-43.png" alt=""/>
+                    <img className="rrss mail" src = "images/rrss-43.png" alt=""></img>
                   </a>
                        
-                  <img className="img_employee" src = "images/maria.jpg" alt=""/>
+                  <img className="img_employee" src = "images/maria.jpg" alt=""></img>
                   <p className="name">María Bolaños</p>
                   <p className="position">Business Consultant</p>
               </div>

a3c701432a92ce11c9b945c3e8830f9e072c9658
lydianm18 <lydia.natour.ext@lafargeholcim.com>
2020-04-03 14:07:15 +0200

    sort lang

--- src/components/Language/LanguageSelector.js
+++ src/components/Language/LanguageSelector.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { languageOptions } from "./Langs/langs";
 import { LanguageContext, isBrowser } from "./LanguageContext";
 
@@ -12,32 +12,33 @@ export const LanguageSelector = () => {
     languageContext.updateLanguage(selectedLanguage.id);
     localStorage.setItem("lang", selectedLanguage.id);
   };
+  const compareValues = (key, order = "asc") => {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
+      const comparison = a[key].localeCompare(b[key]);
+
+      return order === "desc" ? comparison * -1 : comparison;
+    };
+  };
+
+  let order = languageContext.name === "es" ? "desc" : "asc";
+  let opt = languageOptions.sort(compareValues("id", order));
 
   return (
     <select
       onChange={handleLanguageChange}
       value={
         isBrowser() && window.localStorage.getItem("lang")
-          ? window.localStorage.getItem("lang")
+          ? isBrowser() && window.localStorage.getItem("lang")
           : languageContext.name
       }
       className="select-leng"
     >
-      {languageOptions.map(item => {
-        if (languageContext.name === item.id) {
-          return (
-            <option key={item.id} value={item.id} selected>
-              {item.text}
-            </option>
-          );
-        } else {
-          return (
-            <option key={item.id} value={item.id}>
-              {item.text}
-            </option>
-          );
-        }
-      })}
+      {opt.map(item => (
+        <option key={item.id} value={item.id}>
+          {item.text}
+        </option>
+      ))}
     </select>
   );
 };
